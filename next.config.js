b83b98ad9514d307b4926f79f1762f4acb82d762
mohammadak95 <158298916+mohammadak95@users.noreply.github.com>
2024-09-16 15:53:14 +0300

    Configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true },
+    assetPrefix: '/yemen-market-analysis-final/',
+    basePath: '/yemen-market-analysis-final',
+  }
+  
+  module.exports = nextConfig
