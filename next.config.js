fb8aceb33bdf1d83f5f3f9d28b5ef9a91ac663bd
mohammadak95 <158298916+mohammadak95@users.noreply.github.com>
2024-09-17 10:58:09 +0300

    Fix build process for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
